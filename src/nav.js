2887fec567ee6f2af243499ec5378511694a2826
Ruslan Kalashnikov <ruslan.avtomatov@gmail.com>
2020-12-12 15:56:32 -0800

    Refactored the navbar

--- src/nav.js
+++ src/nav.js
@@ -14,14 +14,13 @@ function createNavBar() {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="./index.html">Navigation</a>
+      <a class="navbar-brand" href="./index.html">Home</a>
     </div>
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
-      <li class="active"><a href="./">Home</a></li>
-      <li><a href="providers.html">All Providers</a></li>
-      <li><a href="provider.html">All Provider Details</a></li>
-      <li><a href="about.html">About</a></li>
+      <li><a href="./providers.html">Providers</a></li>
+      <li><a href="./provider.html">Provider</a></li>
+      <li><a href="./about.html">About</a></li>        
       </ul>
     </div>
     <!--/.nav-collapse -->
